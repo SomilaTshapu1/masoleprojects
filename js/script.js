d3f2fc6f3a4f7cdaecf25ef50d0998bd32737d1e
Somila Tshapu <82814273+SomilaTshapu1@users.noreply.github.com>
2022-08-06 11:03:38 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+   contactInfo.classList.remove('active');
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+
+
+
